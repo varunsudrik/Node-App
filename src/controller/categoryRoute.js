27e1dc70c7eb797dbd234d4a6d33121d58dd73cf
Varun Sudrik <varunsudrik99@gmail.com>
2023-04-09 17:38:31 +0530

    added routes

--- src/controller/categoryRoute.js
+++ src/controller/categoryRoute.js
@@ -4,20 +4,31 @@ let categoryRouter = express.Router();
 let category = [
   {
     id: 1,
-    category: "footwear",
+    category: "Fashion",
+    thumb: "https://i.ibb.co/56VP0Fn/cloths.jpg",
   },
   {
     id: 2,
-    category: "fashion",
+    category: "Electronics",
+    thumb: "https://i.ibb.co/pw5Wtdx/appliances.jpg",
   },
   {
     id: 3,
-    category: "gadgets",
+    category: "Essentials",
+    thumb: "https://i.ibb.co/0cw34xm/essentials.jpg",
+  },
+  {
+    id: 4,
+    category: "Footwear",
+    thumb: "https://i.ibb.co/r3SZq8S/footware.jpg",
   },
 ];
 
 categoryRouter.route("/").get((req, res) => {
-  res.send(category);
+  res.render("category", {
+    title: "fwe",
+    data: category,
+  });
 });
 
 export default categoryRouter;
