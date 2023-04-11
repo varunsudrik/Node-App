3e51b2dfcd93fbc57831ef59313dad6f2b1976db
Varun Sudrik <varunsudrik99@gmail.com>
2023-04-11 23:32:57 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 import express from "express";
+import mongoose from "mongoose";
 let app = express();
 import productRouter from "./src/controller/productRoute.js";
 import categoryRouter from "./src/controller/categoryRoute.js";
 import path from "path";
 app.use(express.static(path.join(path.resolve() + "/public")));
-console.log(path.join(path.resolve() + "/public"));
+
+// created connection
+mongoose
+  .connect("mongodb://localhost:27017/kiki")
+  .then(console.log("Db Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+// create schema
+const playlistSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  member: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+// create collection
+const playlist = new mongoose.model("playlist", playlistSchema);
+
+// create document
+
+const createDocument = async () => {
+  const reactPlaylist = new playlist({
+    name: "react-playlist",
+    age: 22,
+    member: false,
+  });
+  const nodePlaylist = new playlist({
+    name: "node ",
+    age: 22,
+    member: false,
+  });
+
+  await reactPlaylist.save();
+};
+createDocument();
+let routes = [
+  {
+    path: "/",
+    key: "Home",
+  },
+  {
+    path: "/category",
+    key: "Category",
+  },
+  {
+    path: "/products",
+    key: "Products",
+  },
+];
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
@@ -13,7 +70,7 @@ app.use("/products", productRouter);
 app.use("/category", categoryRouter);
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "My title", name: "rahul" });
+  res.render("index", { title: "My title", name: "rahul", routes });
 });
 
 app.listen(4000);
--- src/controller/categoryRoute.js
+++ src/controller/categoryRoute.js
@@ -1,34 +1,39 @@
 import express from "express";
 let categoryRouter = express.Router();
-
-let category = [
-  {
-    id: 1,
-    category: "Fashion",
-    thumb: "https://i.ibb.co/56VP0Fn/cloths.jpg",
-  },
-  {
-    id: 2,
-    category: "Electronics",
-    thumb: "https://i.ibb.co/pw5Wtdx/appliances.jpg",
-  },
-  {
-    id: 3,
-    category: "Essentials",
-    thumb: "https://i.ibb.co/0cw34xm/essentials.jpg",
-  },
-  {
-    id: 4,
-    category: "Footwear",
-    thumb: "https://i.ibb.co/r3SZq8S/footware.jpg",
-  },
-];
-
-categoryRouter.route("/").get((req, res) => {
-  res.render("category", {
-    title: "fwe",
-    data: category,
+import mongoose from "mongoose";
+let app = express();
+mongoose
+  .connect("mongodb://localhost:27017/dodo")
+  .then(console.log("connects"))
+  .catch((err) => {
+    console.log(err);
   });
-});
+
+// create Schema object
+const newSchema = new mongoose.Schema([
+  {
+    id: Number,
+    category: String,
+    thubm: String,
+  },
+]);
+
+// create Collection object
+const category = mongoose.model("category", newSchema);
+
+// function to read collection
+const getDocument = async () => {
+  const result = await category.find();
+  console.log(result);
+
+  categoryRouter.route("/").get((req, res) => {
+    res.render("category", {
+      title: "fwe",
+      data: result,
+    });
+  });
+};
+
+getDocument();
 
 export default categoryRouter;
